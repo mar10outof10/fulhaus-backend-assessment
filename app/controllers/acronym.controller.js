53741d1ea8fb0edcd40a794649080b28fc9b824b
Martin Halas <marhaldev@gmail.com>
2022-05-07 14:23:46 -0400

    Setup POST controller

--- app/controllers/acronym.controller.js
+++ app/controllers/acronym.controller.js
@@ -1,14 +1,34 @@
-const Aconym = require('../models/acronym.model.js');
+const Acronym = require('../models/acronym.model.js');
 
 exports.get = (req, res) => {
-  page = req.query.page;
-  limit = req.query.limit;
-  search = req.query.search;
-  
+
+  page = req.query.page || 1;
+  limit = req.query.limit || 10;
+  search = req.query.search || null;
+
+
 }
 
 exports.post = (req, res) => {
+  if(!req.body.acronym || !req.body.definition) {
+      return res.status(400).send({
+          message: "Acronym and Definition must be entered"
+      });
+  }
 
+  const acronym = new Acronym({
+    acronym: req.body.acronym,
+    definition: req.body.definition
+  });
+
+  acronym.save()
+  .then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || "Error occured while creating acronym"
+    })
+  })
 }
 
 exports.patch = (req, res) => {
