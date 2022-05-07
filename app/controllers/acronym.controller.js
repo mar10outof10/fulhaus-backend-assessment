d56e050abd91413a03b7c4c7c3901c8815f4d1f6
Martin Halas <marhaldev@gmail.com>
2022-05-07 14:27:43 -0400

    Added update controller

--- app/controllers/acronym.controller.js
+++ app/controllers/acronym.controller.js
@@ -33,7 +33,33 @@ exports.post = (req, res) => {
 
 exports.patch = (req, res) => {
   const acronymID = req.params.acronymID;
+  if (!req.body.acronym || !req.body.definition) {
+    return res.status(400).send({
+      message: "Acronym and Definition must be entered",
+    });
+  }
 
+  Acronym.findByIdAndUpdate(acronymID, {
+      acronym: req.body.acronym,
+      definition: req.body.definition
+  }, {new: true})
+  .then(acronym => {
+      if(!acronym) {
+          return res.status(404).send({
+              message: `No acronym with ${acronymID} found`
+          });
+      }
+      res.send(acronym);
+  }).catch(err => {
+      if(err.kind === 'ObjectId') {
+          return res.status(404).send({
+              message: `No acronym with ${acronymID} found`
+          });                
+      }
+      return res.status(500).send({
+          message: `Error updating acronym at ${acronymID}`
+      });
+  });
 }
 
 exports.delete = (req, res) => {
