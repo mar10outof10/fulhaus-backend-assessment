5fb34adbd264e75b3cd97484d3f8611e9fc20603
Martin Halas <marhaldev@gmail.com>
2022-05-07 15:13:50 -0400

    Setup seed data

--- app/seeds.js
+++ app/seeds.js
@@ -1,6 +1,6 @@
-const dbConfig = require('./config/database.config.js');
+const dbConfig = require('../config/database.config.js');
 const mongoose = require('mongoose');
-const Acronym = require('./models/acronym');
+const Acronym = require('./models/acronym.model.js');
 
 mongoose.connect(dbConfig.url, {
   useNewUrlParser: true
@@ -13,8 +13,56 @@ mongoose.connect(dbConfig.url, {
 
 const seedAcronyms = [
   {
-    acronym: "",
-    definition: ""
+    acronym: "AATK",
+    definition: "Always at the keyboard"
+  },
+  {
+    acronym: "AAYF",
+    definition: "As always your friend"
+  },
+  {
+    acronym: "ABC",
+    definition: "Already been chewed"
+  },
+  {
+    acronym: "ABD",
+    definition: "Already been done"
+  },
+  {
+    acronym: "ABU",
+    definition: "All bugged up"
+  },
+  {
+    acronym: "AC",
+    definition: "Acceptable content"
+  },
+  {
+    acronym: "ACC",
+    definition: "Anyone can come"
+  },
+  {
+    acronym: "ADBB",
+    definition: "All done bye bye"
+  },
+  {
+    acronym: "ADAD",
+    definition: "Another day another dollar"
+  },
+  {
+    acronym: "BOB",
+    definition: "Back off buddy"
+  },
+  {
+    acronym: "BOL",
+    definition: "Best of luck"
+  },
+  {
+    acronym: "BOLO",
+    definition: "Be on the look out"
+  },
+  {
+    acronym: "BOOMS",
+    definition: "Bored out of my skull"
   },
 ]
 
@@ -24,5 +72,6 @@ const seedDB = async () => {
 };
 
 seedDB().then(() => {
+  console.log('Seed Successful');
   mongoose.connection.close();
 })
